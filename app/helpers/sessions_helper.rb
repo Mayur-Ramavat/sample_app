07c830932d6d06970549d91a54272fa065e5852e
mayur <mayurramavat@gmail.com>
2013-02-08 03:12:00 +0530

    9 complete

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,7 @@ module SessionsHelper
     self.current_user= user
   end
   
-  def current_user=(user)
+  def current_user= (user)
     @current_user = user
   end
   
@@ -17,8 +17,20 @@ module SessionsHelper
   end
   
   def sign_out
-    self.current_usr= nil
+    self.current_user= nil
     cookies.delete(:remember_token)
   end
 
+  def current_user? (user)
+    user == current_user
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
 end
